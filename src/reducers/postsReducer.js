5aa4b81147eba8bf3c0410af4e6e8f98ae27fa0e
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-23 11:42:02 -0400

    refactored reducer to switch statemtne

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const postReducer = (state = [], action) => {
-    if(action.type === 'FETCH_POSTS') {
-        return action.payload;
-    }
-    return state;
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_POSTS':
+            return {action.payload}
+    }    
 };
-
-export default postReducer;
 
